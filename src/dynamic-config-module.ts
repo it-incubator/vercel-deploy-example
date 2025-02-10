54e39f7462e7995d69051e56cbfc6c6f0d6847ea
kuzyuberdin <kuzyuberdin@gmail.com>
2025-02-11 00:41:34 +0400

    copy env files to dist

--- src/dynamic-config-module.ts
+++ src/dynamic-config-module.ts
@@ -1,17 +1,19 @@
 import { ConfigModule } from '@nestjs/config';
+import { join } from 'path';
 
 console.log('Module CONFIG CREATING');
 console.log('process.env.NODE_ENV: ' + process.env.NODE_ENV);
 console.log('process.env.MONGO_URI: ' + process.env.MONGO_URI);
+console.log(join(__dirname, '../envs/.env.production'));
 // you must import this const in the head of your app.module.ts
 export const configModule = ConfigModule.forRoot({
   envFilePath: [
-    process.env.ENV_FILE_PATH?.trim() || '',
-    `.env.${process.env.NODE_ENV}.local`,
-    `.env.${process.env.NODE_ENV}`, // и могут быть переопределены выше стоящими файлами
-    'config.production', // сначала берутся отсюда значение
+  //  process.env.ENV_FILE_PATH?.trim() || '',
+  //  `.env.${process.env.NODE_ENV}.local`,
+  //  `.env.${process.env.NODE_ENV}`, // и могут быть переопределены выше стоящими файлами
+   join(__dirname, './envs/.env.production')
   ],
-  isGlobal: true,
+  isGlobal: true
 });
 
 console.log('Module CONFIG CREATED');
